c171b4b0159f867e2f130536b1a97a9a60bb62e8
Timemash <timemash24@gmail.com>
2022-06-04 18:53:22 +0900

    Add data.json Add clothes images Update to load data.json and filter w/ btns in main.js Update style.css

--- js/main.js
+++ js/main.js
@@ -1,43 +1,92 @@
-const clothesObj = [
-  { type: 'top', sex: 'female', size: 'large', color: 'yellow' },
-  { type: 'skirts', sex: 'female', size: 'small', color: 'red' },
-  { type: 'trousers', sex: 'female', size: 'large', color: 'red' },
-  { type: 'top', sex: 'male', size: 'large', color: 'yellow' },
-  { type: 'top', sex: 'female', size: 'small', color: 'blue' },
-  { type: 'skirts', sex: 'female', size: 'small', color: 'yellow' },
-  { type: 'skirts', sex: 'female', size: 'large', color: 'yellow' },
-  { type: 'trousers', sex: 'male', size: 'large', color: 'blue' },
-  { type: 'trousers', sex: 'male', size: 'large', color: 'red' },
-  { type: 'trousers', sex: 'female', size: 'small', color: 'blue' },
-  { type: 'top', sex: 'male', size: 'small', color: 'yellow' },
-  { type: 'trousers', sex: 'female', size: 'small', color: 'blue' },
-  { type: 'top', sex: 'male', size: 'small', color: 'red' },
-  { type: 'skirts', sex: 'male', size: 'large', color: 'red' },
-  { type: 'skirts', sex: 'male', size: 'large', color: 'blue' },
-];
+const HIDDEN_CLASS = 'hidden';
 
-const clothesList = document.querySelector('.clothes_list');
-const topBtn = document.querySelector('#top');
-const trousersBtn = document.querySelector('#trousers');
-const skirtsBtn = document.querySelector('#skirts');
-const blueBtn = document.querySelector('blue');
-const yellowBtn = document.querySelector('yellow');
-const redBtn = document.querySelector('red');
-const menuBtns = document.querySelectorAll('.menu button');
+// *Fetch the items from the JSON file
+function loadItems() {
+  return fetch('data/data.json')
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
 
-const CLOTHES_CLASS = 'clothes';
-const HIDDEN = 'hidden';
+// *Update the list with the given items
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
+}
 
-clothesObj.forEach((clothes) => {
-  const li = document.createElement('li');
-  li.innerText = `${clothes.type}, ${clothes.color}, ${clothes.sex}, ${clothes.size}`;
-  li.classList.add(CLOTHES_CLASS);
-  const keys = Object.keys(clothes);
-  keys.forEach((key) => {
-    li.classList.add(clothes[key]);
+// *Create HTML list item from the given data item
+function createHTMLString(items) {
+  return `
+  <li class="item">
+    <img src="${items.image}" alt="${items.type}" class="item__thumbnail" />
+      <span class="item__description">${items.gender}, ${items.size}</span>
+  </li>
+  `;
+}
+
+// *Handle button click
+function onButtonClick(event, items) {
+  const target = event.target;
+  const key = target.dataset.key;
+  const value = target.dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+  upadateItems(items, key, value);
+  // displayItems(items.filter((item) => item[key] === value));
+}
+
+// *Make the items matching {key:value} hidden
+function upadateItems(items, key, value) {
+  const liItems = document.querySelectorAll('.item');
+  liItems.forEach((item) => {
+    console.log(item.innerHTML);
+    const info = item.innerHTML;
+    if (info.includes(value)) {
+      item.classList.remove(HIDDEN_CLASS);
+    } else {
+      item.classList.add(HIDDEN_CLASS);
+    }
   });
-  clothesList.appendChild(li);
-});
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', (event) => onButtonClick(event, items));
+}
+
+loadItems()
+  .then((items) => {
+    displayItems(items);
+    setEventListeners(items);
+  })
+  .catch(console.log);
+
+// ! 이전코드
+// const clothesList = document.querySelector('.clothes_list');
+// const topBtn = document.querySelector('#top');
+// const trousersBtn = document.querySelector('#trousers');
+// const skirtsBtn = document.querySelector('#skirts');
+// const blueBtn = document.querySelector('blue');
+// const yellowBtn = document.querySelector('yellow');
+// const redBtn = document.querySelector('red');
+// const menuBtns = document.querySelectorAll('.menu button');
+
+// const CLOTHES_CLASS = 'clothes';
+// const HIDDEN = 'hidden';
+
+// clothesObj.forEach((clothes) => {
+//   const li = document.createElement('li');
+//   li.innerText = `${clothes.type}, ${clothes.color}, ${clothes.sex}, ${clothes.size}`;
+//   li.classList.add(CLOTHES_CLASS);
+//   const keys = Object.keys(clothes);
+//   keys.forEach((key) => {
+//     li.classList.add(clothes[key]);
+//   });
+//   clothesList.appendChild(li);
+// });
 
 function handleBtnClick(event) {
   const clickedBtn = event.target.id;
@@ -57,6 +106,6 @@ function handleBtnClick(event) {
   });
 }
 
-menuBtns.forEach((btn) => {
-  btn.addEventListener('click', handleBtnClick);
-});
+// menuBtns.forEach((btn) => {
+//   btn.addEventListener('click', handleBtnClick);
+// });
